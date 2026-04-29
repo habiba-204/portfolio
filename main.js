86be16b981cf55d86286024f89d71c0771a2dea7
habiba elarabi <habibaelarabi419@gmail.com>
2026-04-30 01:29:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ if(icon.classList.contains("open"))
 }
 }
 icon.addEventListener("click" ,menuToggle);
+
+function showTime() {
+  let now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let time = hours + ":" + minutes + ":" + seconds;
+
+let clock = document.querySelector("#time").innerText =time;
+
+}
+
+showTime();
+
+setInterval(showTime, 1000);
